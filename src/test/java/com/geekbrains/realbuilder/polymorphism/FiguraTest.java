0ccd3fc1b8b7f0805ab9c4433040f96a58578243
Natalya <natsnegireva@gmail.com>
2022-05-21 08:48:02 +0300

    Урок 1

--- src/test/java/com/geekbrains/realbuilder/polymorphism/FiguraTest.java
+++ src/test/java/com/geekbrains/realbuilder/polymorphism/FiguraTest.java
@@ -1,14 +1,6 @@
 package com.geekbrains.realbuilder.polymorphism;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class FiguraTest {
 
-    @org.junit.jupiter.api.Test
-    void area() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void printArea() {
-    }
 }
