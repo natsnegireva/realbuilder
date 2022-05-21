9cb583d8d577d84ab3729835d9bd67570643e51b
Natalya <natsnegireva@gmail.com>
2022-05-21 08:47:56 +0300

    Урок 1

--- src/main/java/com/geekbrains/realbuilder/codeArr/Lorry.java
+++ src/main/java/com/geekbrains/realbuilder/codeArr/Lorry.java
@@ -1,16 +1,16 @@
-package com.geekbrains.realbuilder.codeArr;
-
-public class Lorry extends Car, Moveable, Stopable {
-    @Override
-    void open() {}
-
-    @Override
-    public void move() {
-        System.out.println("Car is moving");
-    }
-
-    @Override
-    public void stop() {
-        System.out.println("Car is stop");
-    }
-}
+//package com.geekbrains.realbuilder.codeArr;
+//
+//public class Lorry extends Car, Moveable, Stopable {
+//    @Override
+//    void open() {}
+//
+//    @Override
+//    public void move() {
+//        System.out.println("Car is moving");
+//    }
+//
+//    @Override
+//    public void stop() {
+//        System.out.println("Car is stop");
+//    }
+//}
